6523757f2f2a3dd606d77330bf8ea89c57564523
BabulHoda98 <babulhoda98@gmail.com>
2025-03-15 14:15:42 +0530

    updated data

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -4,7 +4,9 @@ export default function Plans() {
   return (
     <div className='py-[100px] px-2'>
         <div className='max-w-[1240px] mx-auto md:grid grid-cols-3 gap-6'>
-            <div className='shadow-xl h-[500px] my-4 bg-gray-100 hover:scale-105 duration-500' >First</div>
+            <div className='shadow-xl h-[500px] my-4 bg-gray-100 hover:scale-105 duration-500' >Firstm
+              fjg;lddkgpozdmfv.,z,bc,l;bm;lz/.vzkxmbl;m.
+            </div>
             <div className='shadow-xl h-[500px] my-4 bg-gray-100 hover:scale-105 duration-500' >Second</div>
             <div className='shadow-xl h-[500px] my-4 bg-gray-100 hover:scale-105 duration-500' >Third</div>
         </div>
